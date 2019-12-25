9e377e7a749f9efed67afd905f8184cffaa6bf9b
diptangsu_goswami <diptangsu_goswami@HJD22846>
2019-12-25 19:18:16 +0530

    added gitignore

---
+++ SignalR/signalr/hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.signalr.hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.  
+            //Clients.All.broadcastMessage(name, message);
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
